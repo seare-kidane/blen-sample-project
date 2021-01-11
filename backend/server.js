f2dd83dd37ec4b4a33172837ae79a089d2c43e5b
Seare Kidane <seare.amdemariam@gmail.com>
2021-01-10 19:43:31 -0500

    Added backend cors support to allow access from diff url

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
 const path = require('path');
@@ -12,21 +13,22 @@ const { clients } = JSON.parse(fileContent);
 
 // The GraphQL schema in string form
 const typeDefs = `
-  type Query { clients: [Client], additionalInfo(id: ID!): AdditionalInfo }
+  type Query { clients: [Client], fullInfo(id: ID!): FullInfo }
   type Client { id: ID, age: Int, name: String }
   type AdditionalInfo { gender: String, company: String, email: String, phone: String, address: String }
+  type FullInfo { id: ID, age: Int, name: String, additionalInfo: AdditionalInfo }
 `;
 
 // The resolvers
 const resolvers = {
   Query: {
     clients: () => clients,
-    additionalInfo: (_, { id }) => {
+    fullInfo: (_, { id }) => {
       const client = clients.find(client => client.id === id);
       if (!client) {
         throw new Error(`Couldn't find client with id ${id}`);
       }
-      return client.additionalInfo;
+      return client;
     }
   },
 };
@@ -39,6 +41,9 @@ const schema = makeExecutableSchema({
 
 // Initialize the app
 const app = express();
+
+// not having cors enabled will cause an access control error
+app.use(cors());
 
 // The GraphQL endpoint
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
